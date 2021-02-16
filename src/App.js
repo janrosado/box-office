910f8c182cf48f27f8c2142a871b669215c4be5f
Jan <janmarcopr@hotmail.com>
2021-02-16 18:29:30 -0500

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      hi
-    </div>
+    <Switch>
+      <Route exact path="/">
+          This is home page.
+      </Route>
+      <Route exact path="/starred">
+          This is starred
+      </Route>
+      <Route>
+        <h1>404 - Not Found.</h1>
+        <p>Oops! Page Not Found.</p>
+      </Route>
+    </Switch>
   );
 }
 
