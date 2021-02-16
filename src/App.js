1dccb4cb175dd881a2fc863e0cae87e7433e93ca
Jan <janmarcopr@hotmail.com>
2021-02-16 18:47:58 -0500

    Add nav bar and create page components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import {Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
   return (
+    <div>
+
+      <Navs />
+
     <Switch>
       <Route exact path="/">
-          This is home page.
+          <Home />
       </Route>
       <Route exact path="/starred">
-          This is starred
+          <Starred />
       </Route>
       <Route>
         <h1>404 - Not Found.</h1>
         <p>Oops! Page Not Found.</p>
       </Route>
     </Switch>
+    </div>
   );
 }
 
