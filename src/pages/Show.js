b8bce10d37c8753217f14bf09f1297e17e862932
Jan <janmarcopr@hotmail.com>
2021-02-17 18:56:01 -0500

    Create show page components and populatethem with data

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,35 +1,36 @@
+/* eslint-disable no-underscore-dangle */
 import React, {useEffect, useReducer} from 'react';
 import {useParams} from 'react-router-dom';
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/config';
 
 const reducer = (prevState, action) => {
     switch(action.type){
         case 'FETCH_SUCCESS':{
-            return {isLoading: false, error: null, show: action.show}
+            return {isLoading: false, error: null, show: action.show};
         }
         case 'FETCH_FAILED': {
-            return {...prevState, isLoading: false, error: action.error}
+            return {...prevState, isLoading: false, error: action.error};
         }
-        default: return prevState
+        default: 
+        return prevState;
     }
 
 }
 
 const initialState = {
     show: null,
-    isloading: true,
+    isLoading: true,
     error: null
 }
 
 const Show = () => {
 
     const {id} = useParams();
-
-    const [{show, isLoading, error}, dispatch] = useReducer(reducer, initialState )
-
-    // const [show, setShow] = useState(null);
-    // const [isLoading, setIsLoading] = useState(true);
-    // const [error, setError] = useState(null);
+    const [{show, isLoading, error}, dispatch] = useReducer(reducer, initialState);
 
     useEffect(()=>{
 
@@ -37,8 +38,8 @@ const Show = () => {
 
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
         .then(results => {
-
             if(isMounted){
+                
                 dispatch({type: 'FETCH_SUCCESS', show: results});
             }
 
@@ -52,10 +53,8 @@ const Show = () => {
 
         return () => {
             isMounted = false;
-        }
+        };
     }, [id]);
-
-    console.log(show);
 
     if(isLoading){
         return <div>Data is being loaded</div>
@@ -66,7 +65,31 @@ const Show = () => {
 
     return(
         <div>
-            Hi
+            <ShowMainData 
+            image={show.image} 
+            name={show.name} 
+            rating={show.rating}
+            summary={show.summary}
+            tags={show.genres}
+            />
+
+            <div>
+                <h2>Details</h2>
+                <Details 
+                status={show.status} 
+                network={show.network} 
+                premiered={show.premiered} />
+            </div>
+
+            <div>
+                <h2>Seasons</h2>
+                <Seasons seasons={show._embedded.seasons} />
+            </div>
+
+            <div>
+                <h2>Cast</h2>
+                <Cast cast={show._embedded.cast}/>
+            </div>
         </div>
     )
 }
