3e48499286d73e3e2ffe25fef8643d5a4d905bc9
Jan <janmarcopr@hotmail.com>
2021-02-17 15:23:03 -0500

    Add loading and header messages to the show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -6,13 +6,41 @@ const Show = () => {
 
     const {id} = useParams();
     const [show, setShow] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(()=>{
+
+        let isMounted = true;
+
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
         .then(results => {
-            setShow(results);
-        })
-    }, [id])
+
+            if(isMounted){
+                setShow(results);
+                setIsLoading(false);
+            }
+
+        }).catch(err =>{
+
+            if(isMounted){
+                setError(err.message);
+                setIsLoading(false);
+            }
+ 
+        });
+
+        return () => {
+            isMounted = false;
+        }
+    }, [id]);
+
+    if(isLoading){
+        return <div>Data is being loaded</div>
+    }
+    if(error){
+        return <div>Error Ocurred: {error}</div>
+    }
 
     return(
         <div>
